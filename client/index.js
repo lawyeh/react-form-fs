1971e3a80dd44e5515dc7d5e3f296f37b1650859
lawyeh391 <lawyeh@protonmail.com>
2022-08-21 17:16:12 -0500

    boilerplate

---
+++ client/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "react-dom";
+import App from './App';
+import { ChakraProvider } from '@chakra-ui/react';
+
+render(
+  <ChakraProvider>
+    <App/>
+  </ChakraProvider>,
+  document.getElementById('root')
+)
---
+++ server/server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
